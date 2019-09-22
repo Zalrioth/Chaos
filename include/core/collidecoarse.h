dc4b03662693e7a1d804b95210982f6f73cb8d2c
Zalrioth <nicholasbedner@gmail.com>
2019-09-22 15:54:56 -0700

    Collide coarse ported

--- include/core/collidecoarse.h
+++ include/core/collidecoarse.h
@@ -19,142 +19,20 @@ struct PotentialContact {
   struct RigidBody* body[2];
 };
 
-union BoundingVolumeClass {
-  int placeholder;
-};
-
 struct BVHNode {
   struct BVHNode* children[2];
-  union BoundingVolumeClass* volume;
+  struct BoundingSphere* volume;
   struct RigidBody* body;
   struct BVHNode* parent;
 };
 
-static inline void bvh_node_init(struct BVHNode* bvh_node, struct BVHNode* parent, union BoundingVolumeClass* volume, struct RigidBody* body) {
-  bvh_node->children[0] = bvh_node->children[1] = NULL;
-  bvh_node->parent = parent;
-  bvh_node->volume = volume;
-  bvh_node->body = body;
-}
-
-static inline bool bvh_node_is_leaf(struct BVHNode* bvh_node) {
-  return (bvh_node->body != NULL);
-}
-
-unsigned getPotentialContacts(PotentialContact* contacts, unsigned limit) const;
-void insert(RigidBody* body, const BoundingVolumeClass& volume);
-~BVHNode();
-bool overlaps(const BVHNode<BoundingVolumeClass>* other) const;
-unsigned getPotentialContactsWith(const BVHNode<BoundingVolumeClass>* other, PotentialContact* contacts, unsigned limit) const;
-void recalculateBoundingVolume(bool recurse = true);
-
-template <class BoundingVolumeClass>
-bool BVHNode<BoundingVolumeClass>::overlaps(const BVHNode<BoundingVolumeClass>* other) const {
-  return volume->overlaps(other->volume);
-}
-
-template <class BoundingVolumeClass>
-void BVHNode<BoundingVolumeClass>::insert(RigidBody* newBody, const BoundingVolumeClass& newVolume) {
-  if (isLeaf()) {
-    children[0] = new BVHNode<BoundingVolumeClass>(this, volume, body);
-    children[1] = new BVHNode<BoundingVolumeClass>(this, newVolume, newBody);
-
-    this->body = NULL;
-
-    recalculateBoundingVolume();
-  }
-
-  else {
-    if (children[0]->volume.getGrowth(newVolume) < children[1]->volume.getGrowth(newVolume)) {
-      children[0]->insert(newBody, newVolume);
-    } else {
-      children[1]->insert(newBody, newVolume);
-    }
-  }
-}
-
-template <class BoundingVolumeClass>
-BVHNode<BoundingVolumeClass>::~BVHNode() {
-  if (parent) {
-    BVHNode<BoundingVolumeClass>* sibling;
-    if (parent->children[0] == this)
-      sibling = parent->children[1];
-    else
-      sibling = parent->children[0];
-
-    parent->volume = sibling->volume;
-    parent->body = sibling->body;
-    parent->children[0] = sibling->children[0];
-    parent->children[1] = sibling->children[1];
-
-    sibling->parent = NULL;
-    sibling->body = NULL;
-    sibling->children[0] = NULL;
-    sibling->children[1] = NULL;
-    delete sibling;
-
-    parent->recalculateBoundingVolume();
-  }
-
-  if (children[0]) {
-    children[0]->parent = NULL;
-    delete children[0];
-  }
-  if (children[1]) {
-    children[1]->parent = NULL;
-    delete children[1];
-  }
-}
-
-template <class BoundingVolumeClass>
-void BVHNode<BoundingVolumeClass>::recalculateBoundingVolume(
-    bool recurse) {
-  if (isLeaf())
-    return;
-
-  volume = BVHNode(
-      children[0]->volume,
-      children[1]->volume);
-
-  if (parent)
-    parent->recalculateBoundingVolume(true);
-}
-
-template <class BoundingVolumeClass>
-unsigned BVHNode<BoundingVolumeClass>::getPotentialContacts(PotentialContact* contacts, unsigned limit) const {
-  if (isLeaf() || limit == 0)
-    return 0;
-
-  return children[0]->getPotentialContactsWith(
-      children[1], contacts, limit);
-}
-
-template <class BoundingVolumeClass>
-unsigned BVHNode<BoundingVolumeClass>::getPotentialContactsWith(const BVHNode<BoundingVolumeClass>* other, PotentialContact* contacts, unsigned limit) const {
-  if (!overlaps(other) || limit == 0)
-    return 0;
-
-  if (isLeaf() && other->isLeaf()) {
-    contacts->body[0] = body;
-    contacts->body[1] = other->body;
-    return 1;
-  }
-
-  if (other->isLeaf() || (!isLeaf() && volume->getSize() >= other->volume->getSize())) {
-    unsigned count = children[0]->getPotentialContactsWith(other, contacts, limit);
-
-    if (limit > count)
-      return count + children[1]->getPotentialContactsWith(other, contacts + count, limit - count);
-    else
-      return count;
-  } else {
-    unsigned count = getPotentialContactsWith(other->children[0], contacts, limit);
-
-    if (limit > count)
-      return count + getPotentialContactsWith(other->children[1], contacts + count, limit - count);
-    else
-      return count;
-  }
-}
+static inline void bvh_node_init(struct BVHNode* bvh_node, struct BVHNode* parent, struct BoundingSphere* volume, struct RigidBody* body);
+static inline void bvh_node_delete(struct BVHNode* bvh_node);
+static inline bool bvh_node_is_leaf(struct BVHNode* bvh_node);
+static inline bool bvh_node_overlaps(struct BVHNode* bvh_node, struct BVHNode* other);
+static inline void bvh_node_insert(struct BVHNode* bvh_node, struct RigidBody* new_body, struct BoundingSphere* new_volume);
+static inline void bvh_node_recalculate_bounding_volume(struct BVHNode* bvh_node, bool recurse);
+static inline unsigned int bvh_node_get_potential_contacts(struct BVHNode* bvh_node, struct PotentialContact* contacts, unsigned int limit);
+static inline unsigned int bvh_node_get_potential_contacts_with(struct BVHNode* bvh_node, struct BVHNode* other, struct PotentialContact* contacts, unsigned int limit);
 
 #endif  // COLLIDE_COARSE_H
--- src/core/collidecoarse.c
+++ src/core/collidecoarse.c
@@ -46,3 +46,118 @@ static inline real bounding_sphere_get_growth(struct BoundingSphere* bounding_sp
 static inline real bounding_sphere_get_size(struct BoundingSphere* bounding_sphere) {
   return ((real)1.333333) * R_PI * bounding_sphere->radius * bounding_sphere->radius * bounding_sphere->radius;
 }
+
+static inline void bvh_node_init(struct BVHNode* bvh_node, struct BVHNode* parent, struct BoundingSphere* volume, struct RigidBody* body) {
+  bvh_node->children[0] = bvh_node->children[1] = NULL;
+  bvh_node->parent = parent;
+  bvh_node->volume = volume;
+  bvh_node->body = body;
+}
+
+// Note: Check deletion on this function
+static inline void bvh_node_delete(struct BVHNode* bvh_node) {
+  if (bvh_node->parent) {
+    struct BVHNode* sibling;
+    if (bvh_node->parent->children[0] == bvh_node)
+      sibling = bvh_node->parent->children[1];
+    else
+      sibling = bvh_node->parent->children[0];
+
+    bvh_node->parent->volume = sibling->volume;
+    bvh_node->parent->body = sibling->body;
+    bvh_node->parent->children[0] = sibling->children[0];
+    bvh_node->parent->children[1] = sibling->children[1];
+
+    sibling->parent = NULL;
+    sibling->body = NULL;
+    sibling->children[0] = NULL;
+    sibling->children[1] = NULL;
+    bvh_node_delete(sibling);
+
+    bvh_node_recalculate_bounding_volume(bvh_node->parent, true);
+  }
+
+  if (bvh_node->children[0]) {
+    bvh_node->children[0]->parent = NULL;
+    bvh_node_delete(bvh_node->children[0]);
+  }
+  if (bvh_node->children[1]) {
+    bvh_node->children[1]->parent = NULL;
+    bvh_node_delete(bvh_node->children[1]);
+  }
+
+  free(bvh_node);
+}
+
+static inline bool bvh_node_is_leaf(struct BVHNode* bvh_node) {
+  return (bvh_node->body != NULL);
+}
+
+static inline bool bvh_node_overlaps(struct BVHNode* bvh_node, struct BVHNode* other) {
+  bounding_sphere_overlaps(bvh_node->volume, other->volume);
+}
+
+static inline void bvh_node_insert(struct BVHNode* bvh_node, struct RigidBody* new_body, struct BoundingSphere* new_volume) {
+  if (bvh_node_is_leaf(bvh_node)) {
+    bvh_node->children[0] = calloc(1, sizeof(struct BVHNode));
+    bvh_node_init(bvh_node->children[0], bvh_node, new_volume, new_body);
+
+    bvh_node->children[1] = calloc(1, sizeof(struct BVHNode));
+    bvh_node_init(bvh_node->children[1], bvh_node, new_volume, new_body);
+
+    bvh_node->body = NULL;
+
+    bvh_node_recalculate_bounding_volume(bvh_node->parent, true);
+  }
+
+  else {
+    if (bounding_sphere_get_growth(bvh_node->children[0]->volume, new_volume) < bounding_sphere_get_growth(bvh_node->children[1]->volume, new_volume))
+      bvh_node_insert(bvh_node->children[0], new_body, new_volume);
+    else
+      bvh_node_insert(bvh_node->children[1], new_body, new_volume);
+  }
+}
+
+static inline void bvh_node_recalculate_bounding_volume(struct BVHNode* bvh_node, bool recurse) {
+  if (bvh_node_is_leaf(bvh_node))
+    return;
+
+  bounding_sphere_init_two(bvh_node->volume, bvh_node->children[0]->volume, bvh_node->children[1]->volume);
+
+  if (bvh_node->parent)
+    bvh_node_recalculate_bounding_volume(bvh_node->parent, true);
+}
+
+static inline unsigned int bvh_node_get_potential_contacts(struct BVHNode* bvh_node, struct PotentialContact* contacts, unsigned int limit) {
+  if (bvh_node_is_leaf(bvh_node) || limit == 0)
+    return 0;
+
+  return bvh_node_get_potential_contacts_with(bvh_node->children[0], bvh_node->children[1], contacts, limit);
+}
+
+static inline unsigned int bvh_node_get_potential_contacts_with(struct BVHNode* bvh_node, struct BVHNode* other, struct PotentialContact* contacts, unsigned int limit) {
+  if (!bvh_node_overlaps(bvh_node, other) || limit == 0)
+    return 0;
+
+  if (bvh_node_is_leaf(bvh_node) && bvh_node_is_leaf(other)) {
+    contacts->body[0] = bvh_node->body;
+    contacts->body[1] = other->body;
+    return 1;
+  }
+
+  if (bvh_node_is_leaf(other) || (!bvh_node_is_leaf(bvh_node) && bounding_sphere_get_size(bvh_node->volume) >= othbounding_sphere_get_size(other->volume))) {
+    unsigned int count = bvh_node_get_potential_contacts_with(bvh_node->children[0], other, contacts, limit);
+
+    if (limit > count)
+      return count + bvh_node_get_potential_contacts_with(bvh_node->children[1], other, contacts + count, limit - count);
+    else
+      return count;
+  } else {
+    unsigned int count = bvh_node_get_potential_contacts_with(bvh_node, other->children[0], contacts, limit);
+
+    if (limit > count)
+      return count + bvh_node_get_potential_contacts_with(bvh_node, other->children[1], contacts + count, limit - count);
+    else
+      return count;
+  }
+}
