a4b994d5f135c2ff17b4186e459daa445d49a52d
Zalrioth <nicholasbedner@gmail.com>
2019-11-01 00:06:50 -0700

    Gen port start

--- include/core/fgen.h
+++ include/core/fgen.h
@@ -2,135 +2,195 @@
 #ifndef FGEN_H
 #define FGEN_H
 
+#include <stdlib.h>
 #include "core/body.h"
 
-// Swith this to a DOB approach
-// TODO: Redo this don't like how this works
-enum ForceType { GRAVITY,
-                 GAME_STATE };
-
-union Force {
-  struct Gravity gravity;
-};
-
-struct ForceGenerator {
-  enum ForceType force_type;
-  void (*update_force)(struct RigidBody*, real);
-};
+#define FORCE_VECTOR_INIT_CAPACITY 4
+#define FORCE_VECTOR_RESIZE_FACTOR 2
 
 struct Gravity {
   vec3 gravity;
 };
 
-gravity_init(struct Gravity* gravity);
-void gravity_update_force(struct RigidBody* body, real duration);
+void gravity_init(struct Gravity* gravity);
+void gravity_update_force(struct Gravity* gravity, struct RigidBody* body, real duration);
 
-class Spring : public ForceGenerator {
-  Vector3 connectionPoint;
-  Vector3 otherConnectionPoint;
-  RigidBody* other;
-  real springConstant;
-  real restLength;
-
- public:
-  Spring(const Vector3& localConnectionPt, RigidBody* other, const Vector3& otherConnectionPt, real springConstant, real restLength);
-
-  virtual void updateForce(RigidBody* body, real duration);
+struct Spring {
+  vec3 connection_point;
+  vec3 other_connection_point;
+  struct RigidBody* other;
+  real spring_constant;
+  real rest_length;
 };
 
-class Explosion : public ForceGenerator, public ParticleForceGenerator {
-  real timePassed;
+void spring_init(struct Spring* spring, real* local_connection_point, struct RigidBody* other, vec3 other_connection_point, real spring_constant, real rest_length);
+void spring_update_force(struct Spring* spring, struct RigidBody* body, real duration);
 
- public:
-  Vector3 detonation;
-  real implosionMaxRadius;
-  real implosionMinRadius;
-  real implosionDuration;
-  real implosionForce;
-  real shockwaveSpeed;
-  real shockwaveThickness;
-  real peakConcussionForce;
-  real concussionDuration;
-  real peakConvectionForce;
-  real chimneyRadius;
-  real chimneyHeight;
-  real convectionDuration;
-
- public:
-  Explosion();
-  virtual void updateForce(RigidBody* body, real duration);
-  virtual void updateForce(Particle* particle, real duration) = 0;
+struct Explosion {
+  vec3 detonation;
+  real implosion_max_radius;
+  real implosion_min_radius;
+  real implosion_duration;
+  real implosion_force;
+  real shockwave_speed;
+  real shockwave_thickness;
+  real peak_concussion_force;
+  real concussion_duration;
+  real peak_convection_force;
+  real chimney_radius;
+  real chimney_height;
+  real convection_duration;
 };
 
-class Aero : public ForceGenerator {
- protected:
-  Matrix3 tensor;
-  Vector3 position;
-  const Vector3* windspeed;
+void explosion_init(struct Explosion* explosion);
+void explosion_update_force(struct Explosion* explosion, struct RigidBody* body, real duration);
 
- public:
-  Aero(const Matrix3& tensor, const Vector3& position, const Vector3* windspeed);
-  virtual void updateForce(RigidBody* body, real duration);
-
- protected:
-  void updateForceFromTensor(RigidBody* body, real duration, const Matrix3& tensor);
+struct Aero {
+  mat3 tensor;
+  vec3 position;
+  // NOTE: Might be dangerous watch
+  vec3* wind_speed;
 };
 
-class AeroControl : public Aero {
- protected:
-  Matrix3 maxTensor;
-  Matrix3 minTensor;
+void aero_init(struct Aero aero, real* tensor, real* position, real* wind_speed);
+void aero_update_force(struct Aero aero, struct RigidBody* body, real duration);
+void aero_update_force_from_tensor(struct Aero aero, struct RigidBody* body, real duration, real* tensor);
+
+struct AeroControl {
+  struct Aero aero;
+  mat3 max_tensor;
+  mat3 min_tensor;
   real controlSetting;
-
- private:
-  Matrix3 getTensor();
-
- public:
-  AeroControl(const Matrix3& base, const Matrix3& min, const Matrix3& max, const Vector3& position, const Vector3* windspeed);
-  void setControl(real value);
-  virtual void updateForce(RigidBody* body, real duration);
 };
 
-class AngledAero : public Aero {
-  Quaternion orientation;
+void aero_control_init(struct AeroControl aero_control, real* base, real* min, real* max, real* position, real* windspeed);
+real* aero_control_get_tensor(struct AeroControl aero_control);
+void aero_control_set_control(struct AeroControl aero_control, real value);
+void aero_update_force(struct AeroControl aero_control, struct RigidBody* body, real duration);
 
- public:
-  AngledAero(const Matrix3& tensor, const Vector3& position, const Vector3* windspeed);
-  void setOrientation(const Quaternion& quat);
-  virtual void updateForce(RigidBody* body, real duration);
+struct AngledAero {
+  struct Aero aero;
+  quaternion orientation;
 };
 
-class Buoyancy : public ForceGenerator {
-  real maxDepth;
+void angled_aero_init(struct AngledAero angled_aero, real* tensor, real* position, real* windspeed);
+void angled_aero_set_orientation(real* quat);
+void angled_aero_update_force(struct AngledAero angled_aero, struct RigidBody* body, real duration);
+
+struct Buoyancy {
+  real max_depth;
   real volume;
-  real waterHeight;
-  real liquidDensity;
-  Vector3 centreOfBuoyancy;
-
- public:
-  Buoyancy(const Vector3& cOfB,
-           real maxDepth, real volume, real waterHeight,
-           real liquidDensity = 1000.0f);
-
-  virtual void updateForce(RigidBody* body, real duration);
+  real water_height;
+  real liquid_density;
+  vec3 centre_of_bouyancy;
 };
 
-class ForceRegistry {
- protected:
-  struct ForceRegistration {
-    RigidBody* body;
-    ForceGenerator* fg;
-  };
+// Note: Default liquid density is 1000.0f
+void buoyancy_init(struct Buoyancy buoyancy, real* c_of_b, real max_depth, real volume, real water_height, real liquid_density);
+void buoyancy_update_force(struct Buoyancy buoyancy, struct RigidBody* body, real duration);
 
-  // NOTE: Use custom data structure for this
-  typedef std::vector<ForceRegistration> Registry;
-  Registry registrations;
+//////////////////////////////////////////////////////
 
- public:
-  void add(RigidBody* body, ForceGenerator* fg);
-  void remove(RigidBody* body, ForceGenerator* fg);
-  void clear();
-  void updateForces(real duration);
+enum ForceType { GRAVITY,
+                 SPRING,
+                 EXPLOSION,
+                 AERO,
+                 AERO_CONTROL,
+                 ANGLED_AERO,
+                 BUOYANCY };
+
+union Force {
+  struct Gravity gravity;
+  struct Spring spring;
+  struct Explosion explosion;
+  struct Aero aero;
+  struct AeroControl aero_control;
+  struct AngledAero angled_aero;
+  struct Buoyancy buoyancy;
 };
+
+struct ForceGenerator {
+  enum ForceType force_type;
+  union Force* force;
+  void (*update_force)(void* force, struct RigidBody*, real);
+};
+
+struct ForceRegistration {
+  struct RigidBody* body;
+  struct ForceGenerator* fg;
+};
+
+//////////////////////////////////////////////////////
+
+struct ForceVector {
+  size_t size;
+  size_t capacity;
+  void* items;
+};
+
+static inline void force_vector_init(struct ForceVector* vector, size_t memory_size) {
+  vector->size = 0;
+  vector->capacity = FORCE_VECTOR_INIT_CAPACITY;
+  vector->items = malloc(sizeof(struct ForceRegistration) * FORCE_VECTOR_INIT_CAPACITY);
+}
+
+static inline void force_vector_delete(struct ForceVector* vector) {
+  free(vector->items);
+}
+
+static inline size_t force_vector_size(struct ForceVector* vector) {
+  return vector->size;
+}
+
+static inline void force_vector_resize(struct ForceVector* vector, size_t capacity) {
+  void* new_items = realloc((char*)vector->items, sizeof(struct ForceRegistration) * capacity);
+
+  if (new_items) {
+    vector->items = new_items;
+    vector->capacity = capacity;
+  }
+}
+
+static inline void force_vector_push_back(struct ForceVector* vector, void* item) {
+  if (vector->capacity == vector->size)
+    vector_resize(vector, vector->capacity * FORCE_VECTOR_RESIZE_FACTOR);
+
+  memcpy((char*)vector->items + (sizeof(struct ForceRegistration) * vector->size), item, sizeof(struct ForceRegistration));
+  vector->size++;
+}
+
+static inline void* force_vector_get(struct ForceVector* vector, size_t index) {
+  if (index >= 0 && index < vector->size)
+    return (char*)vector->items + (sizeof(struct ForceRegistration) * index);
+  return NULL;
+}
+
+static inline void force_vector_remove(struct ForceVector* vector, size_t index) {
+  if (index < 0 || index >= vector->size)
+    return;
+  memmove((char*)vector->items + (sizeof(struct ForceRegistration) * index), (char*)vector->items + (sizeof(struct ForceRegistration) * (index + 1)), sizeof(struct ForceRegistration) * (vector->size - (index + 1)));
+
+  vector->size--;
+
+  if (vector->size > 0 && vector->size == vector->capacity / 4)
+    vector_resize(vector, vector->capacity / 2);
+}
+
+static inline void force_vector_clear(struct ForceVector* vector) {
+  vector->size = 0;
+  vector->capacity = FORCE_VECTOR_INIT_CAPACITY;
+  vector_resize(vector, FORCE_VECTOR_INIT_CAPACITY);
+}
+
+//////////////////////////////////////////////////////
+
+struct ForceRegistry {
+  struct ForceVector registrations;
+};
+
+void force_registry_add(struct ForceRegistry* force_registry, struct RigidBody* body, struct ForceGenerator* fg);
+void force_registry_remove(struct ForceRegistry* force_registry, struct RigidBody* body, struct ForceGenerator* fg);
+void force_registry_clear(struct ForceRegistry* force_registry);
+void force_registry_update_forces(struct ForceRegistry* force_registry, real duration);
 
 #endif  // FGEN_H
--- src/core/fgen.c
+++ src/core/fgen.c
@@ -1,168 +1,169 @@
-#include <core/fgen.hpp>
+#include "core/fgen.h"
 
-using namespace chaos;
+void force_registry_update_forces(struct ForceRegistry* force_registry, real duration) {
+  for (int iterate_num = 0; iterate_num < force_vector_size(force_registry); iterate_num++) {
+    struct ForceRegistration* force_registration = (struct ForceRegistration*)(force_registry + (iterate_num * sizeof(struct ForceRegistration)));
 
-void ForceRegistry::updateForces(real duration)
-{
-    Registry::iterator i = registrations.begin();
-    for (; i != registrations.end(); i++) {
-        i->fg->updateForce(i->body, duration);
+    force_registration->fg->update_force(force_registration->fg->force, force_registration->body, duration);
+
+    /*switch (force_registration->fg->force_type) {
+      case GRAVITY:
+        break;
+      case SPRING:
+        break;
+      case EXPLOSION:
+        break;
+      case AERO:
+        break;
+      case AERO_CONTROL:
+        break;
+      case ANGLED_AERO:
+        break;
+      case BUOYANCY:
+        break;
+    }*/
+  }
+}
+
+void force_registry_add(struct ForceRegistry* force_registry, struct RigidBody* body, struct ForceGenerator* fg) {
+  force_vector_push_back(force_registry, &(struct ForceRegistration){body, fg});
+}
+
+void force_registry_remove(struct ForceRegistry* force_registry, struct RigidBody* body, struct ForceGenerator* fg) {
+  for (int iterate_num = 0; iterate_num < force_vector_size(force_registry); iterate_num++) {
+    struct ForceRegistration* force_registration = (struct ForceRegistration*)(force_registry + (iterate_num * sizeof(struct ForceRegistration)));
+
+    if (force_registration->body == body && force_registration->fg == fg) {
+      force_vector_remove(force_registry, iterate_num);
+      return;
     }
+  }
 }
 
-void ForceRegistry::add(RigidBody* body, ForceGenerator* fg)
-{
-    ForceRegistry::ForceRegistration registration;
-    registration.body = body;
-    registration.fg = fg;
-    registrations.push_back(registration);
+void force_registry_clear(struct ForceRegistry* force_registry) {
+  force_vector_clear(force_registry);
 }
 
-Buoyancy::Buoyancy(const Vector3& cOfB, real maxDepth, real volume,
-    real waterHeight, real liquidDensity /* = 1000.0f */)
-{
-    centreOfBuoyancy = cOfB;
-    Buoyancy::liquidDensity = liquidDensity;
-    Buoyancy::maxDepth = maxDepth;
-    Buoyancy::volume = volume;
-    Buoyancy::waterHeight = waterHeight;
+Buoyancy::Buoyancy(const Vector3& cOfB, real maxDepth, real volume, real waterHeight, real liquidDensity /* = 1000.0f */) {
+  centreOfBuoyancy = cOfB;
+  Buoyancy::liquidDensity = liquidDensity;
+  Buoyancy::maxDepth = maxDepth;
+  Buoyancy::volume = volume;
+  Buoyancy::waterHeight = waterHeight;
 }
 
-void Buoyancy::updateForce(RigidBody* body, real duration)
-{
-    // Calculate the submersion depth
-    Vector3 pointInWorld = body->getPointInWorldSpace(centreOfBuoyancy);
-    real depth = pointInWorld.y;
+void Buoyancy::updateForce(RigidBody* body, real duration) {
+  // Calculate the submersion depth
+  Vector3 pointInWorld = body->getPointInWorldSpace(centreOfBuoyancy);
+  real depth = pointInWorld.y;
 
-    // Check if we're out of the water
-    if (depth >= waterHeight + maxDepth)
-        return;
-    Vector3 force(0, 0, 0);
+  // Check if we're out of the water
+  if (depth >= waterHeight + maxDepth)
+    return;
+  Vector3 force(0, 0, 0);
 
-    // Check if we're at maximum depth
-    if (depth <= waterHeight - maxDepth) {
-        force.y = liquidDensity * volume;
-        body->addForceAtBodyPoint(force, centreOfBuoyancy);
-        return;
-    }
-
-    // Otherwise we are partly submerged
-    force.y = liquidDensity * volume * (depth - maxDepth - waterHeight) / 2 * maxDepth;
+  // Check if we're at maximum depth
+  if (depth <= waterHeight - maxDepth) {
+    force.y = liquidDensity * volume;
     body->addForceAtBodyPoint(force, centreOfBuoyancy);
+    return;
+  }
+
+  // Otherwise we are partly submerged
+  force.y = liquidDensity * volume * (depth - maxDepth - waterHeight) / 2 * maxDepth;
+  body->addForceAtBodyPoint(force, centreOfBuoyancy);
 }
 
 Gravity::Gravity(const Vector3& gravity)
-    : gravity(gravity)
-{
+    : gravity(gravity) {
 }
 
-void Gravity::updateForce(RigidBody* body, real duration)
-{
-    // Check that we do not have infinite mass
-    if (!body->hasFiniteMass())
-        return;
+void Gravity::updateForce(RigidBody* body, real duration) {
+  // Check that we do not have infinite mass
+  if (!body->hasFiniteMass())
+    return;
 
-    // Apply the mass-scaled force to the body
-    body->addForce(gravity * body->getMass());
+  // Apply the mass-scaled force to the body
+  body->addForce(gravity * body->getMass());
 }
 
-Spring::Spring(const Vector3& localConnectionPt,
-    RigidBody* other,
-    const Vector3& otherConnectionPt,
-    real springConstant,
-    real restLength)
-    : connectionPoint(localConnectionPt)
-    , otherConnectionPoint(otherConnectionPt)
-    , other(other)
-    , springConstant(springConstant)
-    , restLength(restLength)
-{
+Spring::Spring(const Vector3& localConnectionPt, RigidBody* other, const Vector3& otherConnectionPt, real springConstant, real restLength)
+    : connectionPoint(localConnectionPt), otherConnectionPoint(otherConnectionPt), other(other), springConstant(springConstant), restLength(restLength) {
 }
 
-void Spring::updateForce(RigidBody* body, real duration)
-{
-    // Calculate the two ends in world space
-    Vector3 lws = body->getPointInWorldSpace(connectionPoint);
-    Vector3 ows = other->getPointInWorldSpace(otherConnectionPoint);
+void Spring::updateForce(RigidBody* body, real duration) {
+  // Calculate the two ends in world space
+  Vector3 lws = body->getPointInWorldSpace(connectionPoint);
+  Vector3 ows = other->getPointInWorldSpace(otherConnectionPoint);
 
-    // Calculate the vector of the spring
-    Vector3 force = lws - ows;
+  // Calculate the vector of the spring
+  Vector3 force = lws - ows;
 
-    // Calculate the magnitude of the force
-    real magnitude = force.magnitude();
-    magnitude = real_abs(magnitude - restLength);
-    magnitude *= springConstant;
+  // Calculate the magnitude of the force
+  real magnitude = force.magnitude();
+  magnitude = real_abs(magnitude - restLength);
+  magnitude *= springConstant;
 
-    // Calculate the final force and apply it
-    force.normalise();
-    force *= -magnitude;
-    body->addForceAtPoint(force, lws);
+  // Calculate the final force and apply it
+  force.normalise();
+  force *= -magnitude;
+  body->addForceAtPoint(force, lws);
 }
 
-Aero::Aero(const Matrix3& tensor, const Vector3& position, const Vector3* windspeed)
-{
-    Aero::tensor = tensor;
-    Aero::position = position;
-    Aero::windspeed = windspeed;
+Aero::Aero(const Matrix3& tensor, const Vector3& position, const Vector3* windspeed) {
+  Aero::tensor = tensor;
+  Aero::position = position;
+  Aero::windspeed = windspeed;
 }
 
-void Aero::updateForce(RigidBody* body, real duration)
-{
-    Aero::updateForceFromTensor(body, duration, tensor);
+void Aero::updateForce(RigidBody* body, real duration) {
+  Aero::updateForceFromTensor(body, duration, tensor);
 }
 
-void Aero::updateForceFromTensor(RigidBody* body, real duration,
-    const Matrix3& tensor)
-{
-    // Calculate total velocity (windspeed and body's velocity).
-    Vector3 velocity = body->getVelocity();
-    velocity += *windspeed;
+void Aero::updateForceFromTensor(RigidBody* body, real duration, const Matrix3& tensor) {
+  // Calculate total velocity (windspeed and body's velocity).
+  Vector3 velocity = body->getVelocity();
+  velocity += *windspeed;
 
-    // Calculate the velocity in body coordinates
-    Vector3 bodyVel = body->getTransform().transformInverseDirection(velocity);
+  // Calculate the velocity in body coordinates
+  Vector3 bodyVel = body->getTransform().transformInverseDirection(velocity);
 
-    // Calculate the force in body coordinates
-    Vector3 bodyForce = tensor.transform(bodyVel);
-    Vector3 force = body->getTransform().transformDirection(bodyForce);
+  // Calculate the force in body coordinates
+  Vector3 bodyForce = tensor.transform(bodyVel);
+  Vector3 force = body->getTransform().transformDirection(bodyForce);
 
-    // Apply the force
-    body->addForceAtBodyPoint(force, position);
+  // Apply the force
+  body->addForceAtBodyPoint(force, position);
 }
 
-AeroControl::AeroControl(const Matrix3& base, const Matrix3& min, const Matrix3& max,
-    const Vector3& position, const Vector3* windspeed)
-    : Aero(base, position, windspeed)
-{
-    AeroControl::minTensor = min;
-    AeroControl::maxTensor = max;
-    controlSetting = 0.0f;
+AeroControl::AeroControl(const Matrix3& base, const Matrix3& min, const Matrix3& max, const Vector3& position, const Vector3* windspeed)
+    : Aero(base, position, windspeed) {
+  AeroControl::minTensor = min;
+  AeroControl::maxTensor = max;
+  controlSetting = 0.0f;
 }
 
-Matrix3 AeroControl::getTensor()
-{
-    if (controlSetting <= -1.0f)
-        return minTensor;
-    else if (controlSetting >= 1.0f)
-        return maxTensor;
-    else if (controlSetting < 0) {
-        return Matrix3::linearInterpolate(minTensor, tensor, controlSetting + 1.0f);
-    } else if (controlSetting > 0) {
-        return Matrix3::linearInterpolate(tensor, maxTensor, controlSetting);
-    } else
-        return tensor;
+Matrix3 AeroControl::getTensor() {
+  if (controlSetting <= -1.0f)
+    return minTensor;
+  else if (controlSetting >= 1.0f)
+    return maxTensor;
+  else if (controlSetting < 0) {
+    return Matrix3::linearInterpolate(minTensor, tensor, controlSetting + 1.0f);
+  } else if (controlSetting > 0) {
+    return Matrix3::linearInterpolate(tensor, maxTensor, controlSetting);
+  } else
+    return tensor;
 }
 
-void AeroControl::setControl(real value)
-{
-    controlSetting = value;
+void AeroControl::setControl(real value) {
+  controlSetting = value;
 }
 
-void AeroControl::updateForce(RigidBody* body, real duration)
-{
-    Matrix3 tensor = getTensor();
-    Aero::updateForceFromTensor(body, duration, tensor);
+void AeroControl::updateForce(RigidBody* body, real duration) {
+  Matrix3 tensor = getTensor();
+  Aero::updateForceFromTensor(body, duration, tensor);
 }
 
-void Explosion::updateForce(RigidBody* body, real duration)
-{
+void Explosion::updateForce(RigidBody* body, real duration) {
 }
